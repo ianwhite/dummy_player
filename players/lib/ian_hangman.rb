c6dd2df56b4209ec6a8824d29f5a4310604ffff5
Ian White <ian.w.white@gmail.com>
2013-03-11 20:41:21 +0000

    Using words to guess freqs

--- players/lib/ian_hangman.rb
+++ players/lib/ian_hangman.rb
@@ -4,6 +4,7 @@ class IanHangman
   def initialize length, words = nil
     words ||= self.class.words
     @words = words.select {|w| w.length == length }
+    @words = @words.map &:downcase
   end
 
   def self.words
@@ -12,19 +13,43 @@ class IanHangman
 
   def answer(state, guesses)
     restrict_words(state)
-    guess = make_guess(guesses)
+    if words.empty?
+      guess = make_freq_guess(guesses)
+    else
+      guess = make_educated_guess(guesses)
+    end
     guess
   end
 
-  def letters
-    %w(e t o a n i s r h d l u c m f y w g p b v k x q j z)
-  end
-
-  def make_guess(guesses)
+  def make_freq_guess(guesses)
+    letters = %w(e t o a n i s r h d l u c m f y w g p b v k x q j z)
     (letters - guesses)[0]
   end
 
   def restrict_words(state)
-    words
+    regexp = state.gsub('_','.')
+    @words = words.select {|w| w =~ %r{^#{regexp}$} }
+  end
+
+  def frequencies_for_words
+    letters = [0] * 26
+    words.each do |word|
+      in_word = word.split(//)
+      in_word.each do |letter|
+        ord = letter.ord - 97
+        letters[ord] += 1
+      end
+    end
+
+    freqs = []
+    ('a'..'z').each_with_index {|l, i| freqs << [l, letters[i]]}
+    freqs
+  end
+
+  def make_educated_guess guesses
+    freqs = frequencies_for_words
+    freqs = freqs.reject {|pair| guesses.include?(pair.first)}
+    freqs = freqs.sort_by {|pair| pair.last }
+    freqs.last.first
   end
 end
--- players/spec/hangman_spec.rb
+++ players/spec/hangman_spec.rb
@@ -7,12 +7,16 @@ describe IanHangman do
 
     subject { hangman }
 
-    it "should have some words" do
+    it "should have some words of the right length" do
       subject.words.should include('zonal')
     end
 
     describe "with words stubbed" do
-      let(:hangman) { described_class.new 5, ['axe', 'oxe', 'black'] }
+      let(:hangman) { described_class.new 5, ['axe', 'oxe', 'black', 'sheep'] }
+
+      it "after should restrict itself to words matching length" do
+        hangman.words.should == ['black', 'sheep']
+      end
 
       describe "answer(state, guesses)" do
         subject { hangman.answer(state, guesses) }
@@ -20,16 +24,31 @@ describe IanHangman do
         let(:state)   { "_____" }
         let(:guesses) { [] }
 
-        it "after should restrict itself to words matching state" do
-          subject
-          hangman.words.should == ['black']
+        describe "with no words available" do
+          let(:hangman) { described_class.new 5, [] }
+
+          context "First time" do
+            it "should return a freq letters" do
+              hangman.answer(state, []).should == 'e'
+              hangman.answer(state, ['e']).should == 't'
+              hangman.answer(state, ['e', 't']).should == 'o'
+            end
+          end
         end
 
-        context "First time" do
-          it "should return a freq letters" do
-            subject.should == 'e'
-            hangman.answer(state, ['e']).should == 't'
-            hangman.answer(state, ['e', 't']).should == 'o'
+        describe "#restrict_words(state)" do
+          it { hangman.restrict_words('_____'); hangman.words.should == ['black', 'sheep'] }
+
+          it "should restrict words by state" do
+            hangman.restrict_words('_l___')
+            hangman.words.should == ['black']
+          end
+        end
+
+        describe "#make_educated_guess" do
+          it "should guess the next best letter based on words" do
+            hangman.make_educated_guess(['b']).should == 'e'
+            hangman.make_educated_guess(['b', 'e', 'h', 'a']).should == 'c'
           end
         end
       end
